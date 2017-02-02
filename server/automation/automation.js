32204e8ec64ba6554195372e01cf1fd4e99579cc
Rens van Woudenberg <rensvanw@outlook.com>
2017-02-02 13:12:16 +0100

    Werkend met automation zonder alarm

--- server/automation/automation.js
+++ server/automation/automation.js
@@ -1,84 +1,110 @@
-
 import Rx from 'rxjs/Rx';
 import request from 'request';
 import settingsSchema from './../api/settings/model/settings-model';
 import mongoose from 'mongoose';
 import Promise from 'bluebird';
+import rp from 'request-promise'
 
 
 let Settings = mongoose.model('Settings', settingsSchema);
 
-export default class automationScript{
+let _livingroom;
+let _settings;
 
+export default class Automation {
 
+    static start() {
 
-static getSettings() {
-    return new Promise((resolve, reject) => {
-        var _query = {};
+        let timer = Rx.Observable.interval(1000);
+        var subscription = timer.subscribe(
+            x => Automation.logic(),
+            e => console.log('onError: %s', e),
+            () => console.log('onCompleted'));
+    }
 
-        Settings
-            .findOne(_query)
-            .exec((err, settings) => {
-                err ? reject(err) :
-                    resolve(settings);
+    static logic() {
+        Automation.requestLivingroom();
+    }
+
+    static requestLivingroom() {
+        var options = {
+            uri: 'http://localhost:8080/api/latestlivingroom',
+            json: true // Automatically parses the JSON string in the response 
+        };
+        rp(options)
+            .then(function (response) {
+                _livingroom = response;
+                Automation.requestSettings(_livingroom);
+            })
+            .catch(function (err) {
+                // API call failed... 
             });
-    });
-}
+    }
 
-static start(){
-    let livingroom;
+    static requestSettings(livingroom) {
+        var options = {
+            uri: 'http://localhost:8080/api/settings',
+            json: true // Automatically parses the JSON string in the response 
+        };
 
-    let timer = Rx.Observable.interval(3000);
-    var subscription = timer.subscribe(
-        x => automationScript.requestSettings(),
-        e => console.log('onError: %s', e),
-        () => console.log('onCompleted'));
-}
-
-
-static requestSettings(){
-        request('http://127.0.0.1:8080/api/settings', function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var settings = JSON.parse(body);
-                if(settings.automation == "1"){
-                    console.log("Automation: ", settings.automation)
-
-                    request('http://127.0.0.1:8080/api/latestlivingroom', function(error, response, body) {
-                        if (!error && response.statusCode == 200) {
-                            let livingroom = JSON.parse(body); 
-                               if(livingroom.tempSensor < settings.minTemp){
-                                    // turn heating on
-                                    console.log("Tempsensor: ", livingroom.tempSensor);
-                                    console.log("minTemp: ", settings.minTemp);
-                               }
-                               else{
-                                   // turn heating off
-                               }
-                               // Licht regelen zodra je wel of niet thuis bent
-                               if((settings.lightDetect == "1") && (settings.sleeping == "0") && (settings.home == "1")){
-                                   if(livingroom.distance <= "17"){
-                                       // turn on lightc
-                                       console.log("lightDetect: ", settings.lightDetect);
-                                    console.log("Sleeping: ", settings.sleeping);
-                                    console.log("Home: ", settings.sleeping);
-                                    
-                                   }
-                                   else{
-                                       // turn off light
-                                   }
-                               }
-
-                              
-                            }
-               
-                    });
+        rp(options)
+            .then(function (response) {
+                _settings = response;
+                _livingroom = livingroom;
+                if (_settings.automation == '1') {
+                    console.log("Starting Automation...");
+                    lightDetect();
+                    tempDetect();
                 }
-                else{
-                    //do nothing
+            })
+            .catch(function (err) {
+                // API call failed... 
+            });
+
+
+        function lightDetect() {
+            if (_settings.lightDetect == '1') {
+                console.log("Moving: ", _livingroom.movingSensor);
+                if (_livingroom.movingSensor < 16) {
+                    console.log("Turning light on, moving detected!");
+                    if (livingroom.lamp == '0') {
+                        request('http://127.0.0.1:8080/api/mqtt/livingroom/lighton', function (error, response, body) {});
+                    }
+
+                } else {
+                    console.log("Turning light off, no moving detected!");
+                    if (livingroom.lamp == '1') {
+                        request('http://127.0.0.1:8080/api/mqtt/livingroom/lightoff', function (error, response, body) {});
+                    }
+
                 }
             }
-        });
+        }
+
+        function tempDetect() {
+            console.log("Temp: ", _livingroom.tempSensor);
+            if (_settings.minTemp <= _livingroom.tempSensor) {
+                console.log("turning off heating");
+                if (livingroom.heating == '1') {
+                    request('http://127.0.0.1:8080/api/mqtt/livingroom/heatingoff', function (error, response, body) {});
+                }
+            } else {
+                console.log("turning on heating");
+                if (livingroom.heating == '0') {
+                    request('http://127.0.0.1:8080/api/mqtt/livingroom/heatingon', function (error, response, body) {});
+                }
+            }
+        }
+
+
+
+
+
+
+
     }
+
+
 
 
 }
